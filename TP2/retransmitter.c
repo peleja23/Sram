8078c4290aeb9479f853a135ae4557699eba93e3
José Peleja <pedro_peleja@hotmail.com>
2024-05-11 23:03:31 +0100

    TP2 Update Time

--- TP2/retransmitter.c
+++ TP2/retransmitter.c
@@ -1,14 +1,16 @@
 #include <winsock2.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define PORT 8080
+#define MAX_BUFFER 1024  // podemos ter de aumentar
 
 void receiveData(){
     WSADATA wsaData;
     SOCKET sock;
     struct sockaddr_in server;
-    char server_reply[200];
+    char server_reply[MAX_BUFFER];
 
     WSAStartup(MAKEWORD(2, 2), &wsaData);
 
@@ -17,15 +19,36 @@ void receiveData(){
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
     server.sin_port = htons(PORT);
 
-    connect(sock, (struct sockaddr *)&server, sizeof(server));
+    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        printf("Connect failed. Error\n");
+        return;
+    }
 
-    int len = recv(sock, server_reply, 200, 0);
-    server_reply[len] = '\0'; // Null-terminate the string
-    printf("Server: %s\n", server_reply);
+    while(1) {
+        int len = recv(sock, server_reply, MAX_BUFFER, 0);
+        if (len > 0) {
+            server_reply[len] = '\0';  // Null-terminate the string for safety
+
+            int A, F;
+            memcpy(&A, server_reply, sizeof(int));
+            memcpy(&F, server_reply + sizeof(int), sizeof(int));
+            char* timeString = server_reply + 2 * sizeof(int);
+            printf("A: %d, F: %d, Time: %s\n", A, F, timeString);
+        } else if (len == 0) {
+            printf("Connection closed\n");
+            break;
+        } else {
+            printf("recv failed: %d\n", WSAGetLastError());
+            break;
+        }
+    }
 
     closesocket(sock);
     WSACleanup();
 }
+
+//TODO sendData
+
 int main(int argc, char* argv[]) {
     receiveData();
-}
+} 
--- TP2/server.c
+++ TP2/server.c
@@ -1,30 +1,61 @@
 #include <winsock2.h>
 #include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <math.h>
 
-void senData(){
+#define PORT 8080
+#define MAX_BUFFER 1024 // // temos de aumentar por causa das imagens
+
+void sendData(int F, int A) {
     WSADATA wsa;
     SOCKET serverSocket, clientSocket;
     struct sockaddr_in server;
-    char *message = "Ola";
-    WSAStartup(MAKEWORD(2,2), &wsa);
+    char buffer[100];  
+    time_t rawtime;
+    struct tm * timeinfo;
 
+    int interval = pow(10, -F) * 1000; // Intervalo em milissegundos
+    char timeFormat[50] = "%H:%M:%S";
+
+    WSAStartup(MAKEWORD(2,2), &wsa);
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(8080);
-
+    server.sin_port = htons(PORT);
     bind(serverSocket, (struct sockaddr *)&server, sizeof(server));
-
     listen(serverSocket, 1);
-
     clientSocket = accept(serverSocket, NULL, NULL);
 
-    send(clientSocket, message, strlen(message), 0);
+    while(1) {
+        time(&rawtime);
+        timeinfo = localtime(&rawtime);
+        strftime(buffer, sizeof(buffer), timeFormat, timeinfo);  // Isto ta mal dado que vai ter de se enviar as imagens msm
+
+        int timeLength = strlen(buffer);
+        int pduLength = sizeof(int) * 2 + timeLength + 1; 
+        char* pdu = (char*) malloc(pduLength);
+        int offset = 0;
+
+        memcpy(pdu + offset, &A, sizeof(int));
+        offset += sizeof(int);
+        memcpy(pdu + offset, &F, sizeof(int));
+        offset += sizeof(int);
+        // Copia a string da hora para o PDU
+        memcpy(pdu + offset, buffer, timeLength + 1);        
+        send(clientSocket, pdu, pduLength, 0);
+        free(pdu);
+        Sleep(interval);
+    }
 
     closesocket(clientSocket);
     closesocket(serverSocket);
     WSACleanup();
 }
+
 int main(int argc, char* argv[]) {
-    senData();
+    //TODO: meter istio a ler de args
+    int F = 1;
+    int A = 2; 
+    sendData(F, A);
 }
