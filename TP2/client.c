277e49197d4a070ab9343ccb12ccd491c814bdbe
peleja23 <pedro_peleja@hotmail.com>
2024-06-22 15:44:44 +0100

    Client comments

--- TP2/client.c
+++ TP2/client.c
@@ -1,3 +1,11 @@
+/*
+    Serviços de Rede & Aplicações Multimédia, TP-2
+    Ano Letivo 2023/2024
+    Gustavo Oliveira - A83582
+    Jose Peleja - A84436
+    Marco Araujo - A89387
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,8 +17,9 @@
 #include <SDL2/SDL_image.h>
 #include <math.h>
 
-#define DIGIT_IMAGE_SIZE 2145
+#define DIGIT_IMAGE_SIZE 2145  // Size of each digit image
 
+// Structure representing a PDU (Protocol Data Unit)
 typedef struct {
     int A;
     int F;
@@ -18,6 +27,7 @@ typedef struct {
     char digitImages[16][DIGIT_IMAGE_SIZE];
 } PDU;
 
+// Global variables for the circular buffer and thread control
 PDU *buffer;
 int bufferSize = 2;
 int bufferHead = 0;
@@ -36,12 +46,17 @@ pthread_mutex_t statsMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t bufferNotEmpty = PTHREAD_COND_INITIALIZER;
 pthread_cond_t bufferNotFull = PTHREAD_COND_INITIALIZER;
 
+/*
+    Function to update the buffer and frame interval based on the A and F values from the PDU.
+*/
 void updateBufferAndInterval(int A, int F) {
     pthread_mutex_lock(&bufferMutex);
 
+    // Calculate the new buffer size and new frame interval
     int newBufferSize = pow(10, F) * A;
     int newFrameInterval = 1000000 / pow(10, F);
 
+    // Update the buffer if the size is different
     if (newBufferSize != bufferSize) {
         PDU *newBuffer = malloc(newBufferSize * sizeof(PDU));
         if (bufferHead <= bufferTail) {
@@ -65,6 +80,9 @@ void updateBufferAndInterval(int A, int F) {
     pthread_mutex_unlock(&bufferMutex);
 }
 
+/*
+    Function running in a separate thread to receive PDUs via UDP.
+*/
 void* receivePDU(void* arg) {
     int udpSocket;
     struct sockaddr_in serverAddr;
@@ -77,7 +95,7 @@ void* receivePDU(void* arg) {
     serverAddr.sin_port = htons(receptionPort);
 
     if (bind(udpSocket, (const struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
-        perror("Erro ao fazer bind do socket");
+        perror("Error binding socket");
         close(udpSocket);
         pthread_exit(NULL);
     }
@@ -122,54 +140,71 @@ void* receivePDU(void* arg) {
     return NULL;
 }
 
-SDL_Texture* loadTextureFromMemory(SDL_Renderer *renderer, const char *data, int size) {
+/*
+    Function to load a texture from memory using SDL.
+    @param renderer - the SDL renderer
+    @param data - the image data in the PDU
+    @param size - the size of the image data
+    @return the SDL texture created or NULL in case of an error
+*/
+SDL_Texture* loadTextureFromPDU(SDL_Renderer *renderer, const char *data, int size) {
     if (size == 0 || data[0] == '\0') {
         return NULL;
     }
-    SDL_RWops *rw = SDL_RWFromConstMem(data, size);
+    SDL_RWops *rw = SDL_RWFromConstMem(data, size);  // Create an SDL_RWops structure from the image data
 
-    SDL_Surface *surface = IMG_Load_RW(rw, 1);
+    SDL_Surface *surface = IMG_Load_RW(rw, 1);  // Load an SDL_Surface from the RWops structure
     if (!surface) {
-        printf("Erro ao carregar imagem da memória: %s\n", IMG_GetError());
+        printf("Error loading image from PDU: %s\n", IMG_GetError());
         return NULL;
     }
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);  // Create a texture from the surface
+    SDL_FreeSurface(surface);  // Free the surface as it is no longer needed
     return texture;
 }
 
+/*
+    Function to display the time from the PDU using SDL.
+    @param pdu - the received PDU
+    @param renderer - the SDL renderer
+    @param textures - array of SDL textures
+*/
 void displayTimeFromPDU(PDU *pdu, SDL_Renderer *renderer, SDL_Texture **textures) {
     SDL_Rect dst;
-    dst.y = 80;
-    dst.w = 40;
-    dst.h = 40;
+    dst.y = 80;  // Set the y position of the destination rectangle
+    dst.w = 40;  // Set the width of the destination rectangle
+    dst.h = 40;  // Set the height of the destination rectangle
     for (int i = 0; i < 16; i++) {
         if (textures[i] != NULL) {
-            SDL_DestroyTexture(textures[i]);
+            SDL_DestroyTexture(textures[i]);  // Destroy the old texture if it exists
         }
-        textures[i] = loadTextureFromMemory(renderer, pdu->digitImages[i], DIGIT_IMAGE_SIZE);
+        textures[i] = loadTextureFromPDU(renderer, pdu->digitImages[i], DIGIT_IMAGE_SIZE);  // Load the new texture from pdu
         if (textures[i] == NULL) {
-            continue;
+            continue;  // Skip if the texture couldn't be loaded or its empty
         }
-        dst.x = 0 + i * 64;
-        SDL_RenderCopy(renderer, textures[i], NULL, &dst);
+        dst.x = 0 + i * 64;  // Set the x position of the destination rectangle
+        SDL_RenderCopy(renderer, textures[i], NULL, &dst);  // Copy the texture to the renderer at the specified position
     }
-    SDL_RenderPresent(renderer);
+    SDL_RenderPresent(renderer);  // Present the renderer, updating the screen with the new content
 }
 
+/*
+    Function running in a separate thread to display the clock using SDL.
+*/
 void* displayClock(void *arg) {
-    SDL_Window *window = SDL_CreateWindow("Relógio Digital", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 200, SDL_WINDOW_SHOWN);
+    SDL_Window *window = SDL_CreateWindow("Digital Clock", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 200, SDL_WINDOW_SHOWN);
     if (window == NULL) {
-        printf("Erro ao criar janela: %s\n", SDL_GetError());
+        printf("Error creating window: %s\n", SDL_GetError());
         pthread_exit(NULL);
     }
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_Texture *textures[16];
-    memset(textures, 0, sizeof(textures));
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);  // Create an accelerated renderer
+    SDL_Texture *textures[16];  // Array to store textures for the digit images
+    memset(textures, 0, sizeof(textures));  // Initialize the texture array to NULL
 
-    int running = 1;
+    int running = 1;  // Flag to control the main loop
     while (running) {
+        // Lock the stats mutex to check the running status
         pthread_mutex_lock(&statsMutex);
         if (!keepRunning) {
             pthread_mutex_unlock(&statsMutex);
@@ -177,25 +212,30 @@ void* displayClock(void *arg) {
         }
         pthread_mutex_unlock(&statsMutex);
 
+        // Lock the buffer mutex to check if there is data to display
         pthread_mutex_lock(&bufferMutex);
         while (bufferHead == bufferTail) {
-            pthread_cond_wait(&bufferNotEmpty, &bufferMutex);
+            pthread_cond_wait(&bufferNotEmpty, &bufferMutex);  // Wait until the buffer is not empty
         }
 
+        // Get the next frame from the buffer
         PDU frame = buffer[bufferHead];
         bufferHead = (bufferHead + 1) % bufferSize;
-        pthread_cond_signal(&bufferNotFull);
+        pthread_cond_signal(&bufferNotFull);  // Signal that the buffer is not full
         pthread_mutex_unlock(&bufferMutex);
-        displayTimeFromPDU(&frame, renderer, textures);
-        usleep(frameInterval);
+
+        displayTimeFromPDU(&frame, renderer, textures);  // Display the time from the PDU
+        usleep(frameInterval);  // Sleep for the frame interval
+
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
-                running = 0;
+                running = 0;  // Exit the loop if the window is closed
             }
         }
     }
 
+    // Clean up textures and renderer
     for (int i = 0; i < 16; i++) {
         if (textures[i]) {
             SDL_DestroyTexture(textures[i]);
@@ -206,12 +246,18 @@ void* displayClock(void *arg) {
     pthread_exit(NULL);
 }
 
+/*
+    Function to read the reception port from a configuration file.
+*/
 void readReceptionPort(const char* filename, int* port) {
     FILE* file = fopen(filename, "r");
     fscanf(file, "%d", port);
     fclose(file);
 }
 
+/*
+    Function running in a separate thread to listen for user input to exit the program.
+*/
 void* listenForExit(void* arg) {
     char input[10];
     while (1) {
@@ -230,29 +276,35 @@ void* listenForExit(void* arg) {
     return NULL;
 }
 
+/*
+    Function to print the transmission statistics.
+*/
 void printStatistics() {
     int expectedFrames = lastFrameCount - firstFrameCount + 1;
     int packetLoss = expectedFrames - framesCounter;
     int packetLossBuffer = expectedFrames - framesCounter + skippedFrames;
     printf("\n--- Statistics ---\n");
-    printf("Total times buffer reseted: %d\n", bufferReset);
+    printf("Total times buffer reset: %d\n", bufferReset);
     printf("Total frames lost without buffer reset: %d\n", packetLoss);
     printf("Total frames lost with buffer reset: %d\n", packetLossBuffer);
 }
 
+/*
+    Main function of the program.
+*/
 int main() {
     pthread_t receiverThread, displayThread, exitThread;
 
-    SDL_Init(SDL_INIT_VIDEO);
-    IMG_Init(IMG_INIT_PNG);
+    SDL_Init(SDL_INIT_VIDEO);  // Initialize the SDL video subsystem
+    IMG_Init(IMG_INIT_PNG);    // Initialize the SDL image subsystem with PNG support
 
-    readReceptionPort("client.txt", &receptionPort);
+    readReceptionPort("client.txt", &receptionPort); // Read the reception port from the configuration file
 
     buffer = malloc(bufferSize * sizeof(PDU));
 
-    pthread_create(&receiverThread, NULL, receivePDU, NULL);
-    pthread_create(&displayThread, NULL, displayClock, NULL);
-    pthread_create(&exitThread, NULL, listenForExit, NULL);
+    pthread_create(&receiverThread, NULL, receivePDU, NULL); // Create the thread to receive PDUs
+    pthread_create(&displayThread, NULL, displayClock, NULL); // Create the thread to display the clock
+    pthread_create(&exitThread, NULL, listenForExit, NULL); // Create the thread to listen for user input to exit
 
     pthread_join(exitThread, NULL);
     pthread_join(receiverThread, NULL);
@@ -260,8 +312,8 @@ int main() {
 
     printStatistics();
 
-    IMG_Quit();
-    SDL_Quit();
+    IMG_Quit();  // Quit the SDL image subsystem
+    SDL_Quit();  // Quit the SDL video subsystem
 
     return 0;
 }
